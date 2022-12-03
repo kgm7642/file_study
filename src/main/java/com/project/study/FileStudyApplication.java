9234739ddedcad429085ba7f5f5366db65ee7f18
KyungMin <kgm7642@gmail.com>
2022-12-03 12:31:22 +0900

    init

---
+++ src/main/java/com/project/study/FileStudyApplication.java
@@ -0,0 +1,13 @@
+package com.project.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStudyApplication.class, args);
+	}
+
+}
